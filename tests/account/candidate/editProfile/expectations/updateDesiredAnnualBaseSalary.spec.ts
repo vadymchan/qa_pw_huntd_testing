46c1999791d259d07a90c5e6372bbcaa37a8bac2
VadymChan <vadymchan@gmail.com>
2026-07-13 08:04:39 +0300

    add linear tests for candidate expectation in edit profile

---
+++ tests/account/candidate/editProfile/expectations/updateDesiredAnnualBaseSalary.spec.ts
@@ -0,0 +1,35 @@
+import { test } from '../../../../_fixtures/fixtures';
+import { graphqlWaitForResponse } from '../../../../../src/utils/playwright/graphqlWaitForResponse';
+import { expect } from '@playwright/test';
+import { generateSalaryString } from '../../../../../src/utils/generators/generateSalaryString';
+
+test.describe(`Edit profile as candidate`, () => {
+  test.beforeEach(async ({ page, registeredCandidate }) => {
+    await page.goto('/sign-in');
+
+    await page.getByLabel('Email').fill(registeredCandidate.userCredentials.email);
+    await page.getByLabel('Password').fill(registeredCandidate.userCredentials.password);
+    await page.getByRole('button', { name: 'Sign In', exact: true }).click();
+    await page.waitForURL('/profile-preview/**');
+  });
+
+  test(`User should update desired annual base salary`, async ({ page }) => {
+    const salaryType = 'Annual';
+    const desiredBaseSalary = 30_000;
+
+    await page.goto('/profile/candidate/job-expectations');
+
+    await page.getByRole('button', { name: salaryType }).click();
+    await page.getByRole('textbox', { name: /Desired base salary/i }).fill(`${desiredBaseSalary}`);
+
+    await graphqlWaitForResponse(page, 'updateCandidateProfile', async () => {
+      await page.getByRole('button', { name: 'Save changes' }).click();
+    });
+
+    await page.goto('/profile-preview/candidate');
+
+    await expect(
+      page.locator('[class*=ProfileMeta_metaWrapper]').getByRole('listitem').nth(2),
+    ).toHaveText(generateSalaryString(salaryType, desiredBaseSalary));
+  });
+});
---
+++ tests/account/candidate/editProfile/expectations/updateEnglishLevel.spec.ts
@@ -0,0 +1,34 @@
+import { test } from '../../../../_fixtures/fixtures';
+import { graphqlWaitForResponse } from '../../../../../src/utils/playwright/graphqlWaitForResponse';
+import { expect } from '@playwright/test';
+
+test.describe(`Edit profile as candidate`, () => {
+  test.beforeEach(async ({ page, registeredCandidate }) => {
+    await page.goto('/sign-in');
+
+    await page.getByLabel('Email').fill(registeredCandidate.userCredentials.email);
+    await page.getByLabel('Password').fill(registeredCandidate.userCredentials.password);
+    await page.getByRole('button', { name: 'Sign In', exact: true }).click();
+    await page.waitForURL('/profile-preview/**');
+  });
+
+  test(`User should update English level`, async ({ page }) => {
+    const englishLevel = 'Pre intermediate';
+
+    await page.goto('/profile/candidate/job-expectations');
+
+    await page.getByLabel('English level').focus();
+    await page.keyboard.press('ArrowDown');
+    await page.locator('.select__option').getByText(englishLevel, { exact: true }).click();
+
+    await graphqlWaitForResponse(page, 'updateCandidateProfile', async () => {
+      await page.getByRole('button', { name: 'Save changes' }).click();
+    });
+
+    await page.goto('/profile-preview/candidate');
+
+    await expect(
+      page.locator('[class*=ProfileMeta_metaWrapper]').getByRole('listitem').nth(3),
+    ).toHaveText(englishLevel);
+  });
+});
