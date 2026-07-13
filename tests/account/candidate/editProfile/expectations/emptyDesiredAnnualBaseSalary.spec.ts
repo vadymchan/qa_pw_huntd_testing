b03bda2b1206505cff460d0d3899bccc0392ad00
VadymChan <vadymchan@gmail.com>
2026-07-13 09:01:32 +0300

    add negative tests for candidate expectations in edit profile page

---
+++ tests/account/candidate/editProfile/expectations/emptyDesiredAnnualBaseSalary.spec.ts
@@ -0,0 +1,31 @@
+import { test } from '../../../../_fixtures/fixtures';
+import { expect } from '@playwright/test';
+
+test.describe(`Edit profile as candidate`, () => {
+  test.beforeEach(async ({ page, registeredCandidate }) => {
+    await page.goto('/sign-in');
+
+    await page.getByLabel('Email').fill(registeredCandidate.userCredentials.email);
+    await page.getByLabel('Password').fill(registeredCandidate.userCredentials.password);
+    await page.getByRole('button', { name: 'Sign In', exact: true }).click();
+    await page.waitForURL('/profile-preview/**');
+  });
+
+  test(`User should see validation error when desired annual base salary is empty`, async ({
+    page,
+  }) => {
+    const salaryType = 'Annual';
+    const desiredBaseSalary = '';
+
+    await page.goto('/profile/candidate/job-expectations');
+
+    await page.getByRole('button', { name: salaryType }).click();
+    await page.getByRole('textbox', { name: /Desired base salary/i }).fill(`${desiredBaseSalary}`);
+
+    await page.getByRole('button', { name: 'Save changes' }).click();
+
+    await expect(page.locator('[class*=FormField_metaBlock]').first()).toHaveText(
+      'Salary is required',
+    );
+  });
+});
