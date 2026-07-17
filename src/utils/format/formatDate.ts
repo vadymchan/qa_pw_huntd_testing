70ca462070a05844cf18548db974e4cbe335c653
VadymChan <vadymchan@gmail.com>
2026-07-17 15:52:09 +0300

    add format date util

---
+++ src/utils/format/formatDate.ts
@@ -0,0 +1,20 @@
+import { Month } from '../../models/auth/candidate/Month';
+
+const MONTH_NUMBERS: Record<Month, number> = {
+  January: 1,
+  February: 2,
+  March: 3,
+  April: 4,
+  May: 5,
+  June: 6,
+  July: 7,
+  August: 8,
+  September: 9,
+  October: 10,
+  November: 11,
+  December: 12,
+};
+
+export function formatDate(month: Month, year: number) {
+  return `${year}-${MONTH_NUMBERS[month]}`;
+}
