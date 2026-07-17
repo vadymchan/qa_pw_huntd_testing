4eb13fc847c0439bced6f0f7e8e7c8411f40983d
VadymChan <vadymchan@gmail.com>
2026-07-17 15:51:44 +0300

    add BasePage

---
+++ src/pages/BasePage.ts
@@ -0,0 +1,21 @@
+import { expect, Page } from '@playwright/test';
+import { testStep } from '../utils/playwright/testStep';
+
+export class BasePage {
+  constructor(
+    protected page: Page,
+    protected url: string, // TODO: consider if we need url contract in BasePage
+  ) {}
+
+  protected async step<T>(title: string, stepToRun: () => Promise<T> | T): Promise<T> {
+    return await testStep(title, stepToRun);
+  }
+
+  async open() {
+    await this.page.goto(this.url);
+  }
+
+  async assertOpened() {
+    await expect(this.page).toHaveURL(this.url);
+  }
+}
