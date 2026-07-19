b16f71485e074c3fa04b1f67343be2d6e070a3fe
VadymChan <vadymchan@gmail.com>
2026-07-19 13:58:18 +0300

    update candidate edit bio profile  tests to modular

--- tests/account/candidate/editProfile/bio/updateAchivements.spec.ts
+++ tests/account/candidate/editProfile/bio/updateAchivements.spec.ts
@@ -1,40 +1,20 @@
-import { faker } from '@faker-js/faker';
 import { test } from '../../../../_fixtures/fixtures';
-import { graphqlWaitForResponse } from '../../../../../src/utils/playwright/graphqlWaitForResponse';
-import { expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 test.describe(`Edit profile as candidate`, () => {
-  test.beforeEach(async ({ page, registeredCandidate }) => {
-    await page.goto('/sign-in');
-
-    await page.getByLabel('Email').fill(registeredCandidate.userCredentials.email);
-    await page.getByLabel('Password').fill(registeredCandidate.userCredentials.password);
-    await page.getByRole('button', { name: 'Sign In', exact: true }).click();
-    await page.waitForURL('/profile-preview/**');
-  });
-
-  test(`User should update achivements`, async ({ page }) => {
+  test(`User should update achivements`, async ({
+    registerNewCandidate,
+    editCandidateProfileBioPage,
+    candidateProfilePreviewPage,
+  }) => {
     const achivements = faker.lorem.sentence();
 
-    await page.goto('/profile/candidate/bio');
+    await editCandidateProfileBioPage.open();
+    await editCandidateProfileBioPage.profileBio.fillAchivements(achivements);
+    const waitForResponse = true;
+    await editCandidateProfileBioPage.clickSaveChanges(waitForResponse);
 
-    await page.getByLabel('Achievements / Key results').fill(achivements);
-
-    await graphqlWaitForResponse(page, 'updateCandidateProfile', async () => {
-      await page.getByRole('button', { name: 'Save changes' }).click();
-    });
-
-    await page.goto('/profile-preview/candidate');
-
-    await expect(
-      page
-        .locator('[class*=ProfileInfo_item]')
-        .filter({
-          has: page
-            .locator('[class*=ProfileInfo_itemTitle]')
-            .getByText('Achievements / Key results'),
-        })
-        .getByRole('definition'),
-    ).toHaveText(achivements);
+    await candidateProfilePreviewPage.open();
+    await candidateProfilePreviewPage.assertAchivementsHaveText(achivements);
   });
 });
--- tests/account/candidate/editProfile/bio/updateExpectationsFromWork.spec.ts
+++ tests/account/candidate/editProfile/bio/updateExpectationsFromWork.spec.ts
@@ -1,38 +1,20 @@
-import { faker } from '@faker-js/faker';
 import { test } from '../../../../_fixtures/fixtures';
-import { graphqlWaitForResponse } from '../../../../../src/utils/playwright/graphqlWaitForResponse';
-import { expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
 test.describe(`Edit profile as candidate`, () => {
-  test.beforeEach(async ({ page, registeredCandidate }) => {
-    await page.goto('/sign-in');
-
-    await page.getByLabel('Email').fill(registeredCandidate.userCredentials.email);
-    await page.getByLabel('Password').fill(registeredCandidate.userCredentials.password);
-    await page.getByRole('button', { name: 'Sign In', exact: true }).click();
-    await page.waitForURL('/profile-preview/**');
-  });
-
-  test(`User should update expectations from work`, async ({ page }) => {
+  test(`User should update expectations from work`, async ({
+    registerNewCandidate,
+    editCandidateProfileBioPage,
+    candidateProfilePreviewPage,
+  }) => {
     const expectations = faker.lorem.sentence();
 
-    await page.goto('/profile/candidate/bio');
+    await editCandidateProfileBioPage.open();
+    await editCandidateProfileBioPage.profileBio.fillWorkExpectations(expectations);
+    const waitForResponse = true;
+    await editCandidateProfileBioPage.clickSaveChanges(waitForResponse);
 
-    await page.getByLabel('Expectations from work (optional)').fill(expectations);
-
-    await graphqlWaitForResponse(page, 'updateCandidateProfile', async () => {
-      await page.getByRole('button', { name: 'Save changes' }).click();
-    });
-
-    await page.goto('/profile-preview/candidate');
-
-    await expect(
-      page
-        .locator('[class*=ProfileInfo_item]')
-        .filter({
-          has: page.locator('[class*=ProfileInfo_itemTitle]').getByText('Job expectations'),
-        })
-        .getByRole('definition'),
-    ).toHaveText(expectations);
+    await candidateProfilePreviewPage.open();
+    await candidateProfilePreviewPage.assertExpectationsHaveText(expectations);
   });
 });
