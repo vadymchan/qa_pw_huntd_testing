442c83a273c4a70ed49b8991a433f54fadfe264f
VadymChan <vadymchan@gmail.com>
2026-07-19 14:01:38 +0300

    add auth setup project

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,40 +37,15 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+{
+      name: 'setup',
+      testMatch: /.*\.setup\.ts/,
+    },
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+    dependencies: ['setup'],
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
---
+++ tests/auth.setup.ts
@@ -0,0 +1,13 @@
+import { test as setup } from './_fixtures/fixtures';
+import path from 'path';
+
+const candidate = path.join(__dirname, '../playwright/.auth/candidate.json');
+const recruiter = path.join(__dirname, '../playwright/.auth/recruiter.json');
+
+setup(`authenticate as candidate`, async ({ context, registerNewCandidate }) => {
+  await context.request.storageState({ path: candidate });
+});
+
+setup(`authenticate as recruiter`, async ({ context, registerNewRecruiter }) => {
+  await context.request.storageState({ path: recruiter });
+});
